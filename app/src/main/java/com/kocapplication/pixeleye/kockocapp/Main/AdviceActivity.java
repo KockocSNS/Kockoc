0c289974fc5ee038351c6c1299400ff9904fbe15
tpriez <dudqja189@gmail.com>
2016-07-11 13:28:38 +0900

    도움말 슬라이드로 변경

--- app/src/main/java/com/kocapplication/pixeleye/kockocapp/Main/AdviceActivity.java
+++ app/src/main/java/com/kocapplication/pixeleye/kockocapp/Main/AdviceActivity.java
@@ -1,11 +1,25 @@
 package com.kocapplication.pixeleye.kockocapp.main;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.kocapplication.pixeleye.kockocapp.R;
 import com.kocapplication.pixeleye.kockocapp.main.help.advice_page_1;
@@ -19,74 +33,153 @@ import com.kocapplication.pixeleye.kockocapp.main.help.advice_page_7;
 import com.kocapplication.pixeleye.kockocapp.main.help.advice_page_8;
 import com.kocapplication.pixeleye.kockocapp.main.help.advice_page_9;
 
-/**
- * Created by Nam on 2016-06-27.
- */
 public class AdviceActivity extends FragmentActivity {
 
-    private int MAX_PAGE=10;
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advice);
 
-        ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
-        viewPager.setAdapter(new adapter(getSupportFragmentManager()));
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        btnSkip = (Button) findViewById(R.id.btn_skip);
+        btnNext = (Button) findViewById(R.id.btn_next);
 
-    }
+        layouts = new int[]{
+                R.layout.activity_adapter_advice1,
+                R.layout.activity_adapter_advice2,
+                R.layout.activity_adapter_advice3,
+                R.layout.activity_adapter_advice4,
+                R.layout.activity_adapter_advice5,
+                R.layout.activity_adapter_advice6,
+                R.layout.activity_adapter_advice7,
+                R.layout.activity_adapter_advice8,
+                R.layout.activity_adapter_advice9,
+                R.layout.activity_adapter_advice10};
 
+        addBottomDots(0);
 
-    private class adapter extends FragmentPagerAdapter {
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
 
-        public adapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        public int getCount() {
-            return MAX_PAGE;
-        }
-
-        public Fragment getItem(int position) {
-            if(position<0 || MAX_PAGE <= position)
-                return  null;
-            Fragment fragment = new Fragment();
-            switch (position) {
-                case 0:
-                    fragment = new advice_page_1();
-                    break;
-                case 1:
-                    fragment = new advice_page_2();
-                    break;
-                case 2:
-                    fragment = new advice_page_3();
-                    break;
-                case 3:
-                    fragment = new advice_page_4();
-                    break;
-                case 4:
-                    fragment = new advice_page_5();
-                    break;
-                case 5:
-                    fragment = new advice_page_6();
-                    break;
-                case 6:
-                    fragment = new advice_page_7();
-                    break;
-                case 7:
-                    fragment = new advice_page_8();
-                    break;
-                case 8:
-                    fragment = new advice_page_9();
-                    break;
-                case 9:
-                    fragment = new advice_page_10();
-                    break;
-
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchHomeScreen();
             }
-            return fragment;
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // checking for last page
+                // if last page home screen will be launched
+                int current = getItem(+1);
+                if (current < layouts.length) {
+                    // move to next screen
+                    viewPager.setCurrentItem(current);
+                } else {
+                    launchHomeScreen();
+                }
+            }
+        });
+    }
+
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[layouts.length];
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.dot_inactive));
+            dotsLayout.addView(dots[i]);
         }
 
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(getResources().getColor(R.color.dot_active));
     }
+
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+
+    private void launchHomeScreen() {
+        finish();
+    }
+
+    //  viewpager change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+
+            // changing the next button text 'NEXT' / 'GOT IT'
+            if (position == layouts.length - 1) {
+                // last page. make button text to GOT IT
+                btnNext.setText(getString(R.string.start));
+                btnSkip.setVisibility(View.GONE);
+            } else {
+                // still pages are left
+                btnNext.setText(getString(R.string.next));
+                btnSkip.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+
 }
 
