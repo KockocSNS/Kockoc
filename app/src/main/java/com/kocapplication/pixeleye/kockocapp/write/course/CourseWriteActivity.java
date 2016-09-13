c571c0bb785f4d9211bb58ae5819b626e5c2f6ca
jeongjiwoo <jiwoo6550@gmail.com>
2016-09-13 17:48:43 +0900

    코스 작성시 공개여부 설정가능

--- app/src/main/java/com/kocapplication/pixeleye/kockocapp/write/course/CourseWriteActivity.java
+++ app/src/main/java/com/kocapplication/pixeleye/kockocapp/write/course/CourseWriteActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -59,7 +60,6 @@ public class CourseWriteActivity extends BaseActivityWithoutNav {
     private Button confirm;
     private CheckBox publicityCheckBox;
 
-    private String memo = "test";
     public static int mNumCheck;
 
     @Override
@@ -219,7 +219,10 @@ public class CourseWriteActivity extends BaseActivityWithoutNav {
                 if(flag == ADJUST_FLAG){
 
                 }else{// 새 코스 쓰기
-                    JspConn.uploadCourse(courseTitle,courses.getCourses());
+                    boolean publicity;
+                    if(publicityCheckBox.isChecked()) publicity = true;
+                    else publicity = false;
+                    JspConn.uploadCourse(courseTitle,courses.getCourses(),publicity);
                 }
 
                 memo = ""; // 메모 초기화
